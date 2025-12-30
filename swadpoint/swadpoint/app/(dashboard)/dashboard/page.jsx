c55887d48608e9ebd307b895949677736d68897b
manasvidobariya82 <manasvidobariya82@gmail.com>
2025-12-30 20:59:23 +0530

    organize structure

--- swadpoint/swadpoint/app/(dashboard)/dashboard/page.jsx
+++
@@ -1,9 +0,0 @@
-import StatsCards from "../../../components/dashboard/StatsCards";
-
-export default function DashboardPage() {
-  return (
-    <div className="space-y-6">
-      <StatsCards />
-    </div>
-  );
-}
